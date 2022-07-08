b3c96e3061ecd29943dfbda69976a8c19472ba20
thegergo02 <work@varigergo.xyz>
2022-07-08 16:05:58 +0200

    ?

--- flake.nix
+++ flake.nix
@@ -6,28 +6,33 @@
   };
 
   outputs = { self, nixpkgs }: {
-		defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
+    defaultPackage.x86_64-linux =
+      with import nixpkgs { 
+        system = "x86_64-linux"; 
+        config = { allowUnfree = true; };
+      };
 
       stdenv.mkDerivation rec {
-						name = "zitadel-${version}";
-						version = "v2.0.0-v2-alpha.33";
+        name = "zitadel-${version}";
+        version = "v2.0.0-v2-alpha.33";
 
-						src = pkgs.fetchurl {
-							url = "https://github.com/zitadel/zitadel/releases/download/${version}/zitadel_Linux_x86_64.tar.gz";
-							sha256 = "sha256-HpLu3cXQ+lUmnvrqjU/SEnGzouSO0ZSiwn1NIBNHSVM=";
-						};
-						sourceRoot = ".";
+        src = pkgs.fetchurl {
+                url = "https://github.com/zitadel/zitadel/releases/download/${version}/zitadel_Linux_x86_64.tar.gz";
+                sha256 = "sha256-HpLu3cXQ+lUmnvrqjU/SEnGzouSO0ZSiwn1NIBNHSVM=";
+        };
+        sourceRoot = ".";
+        
+        buildInputs = [ cockroachdb ];
 
-						installPhase = ''
-						install -m755 -D zitadel $out/bin/zitadel
-						'';
+        installPhase = ''
+        install -m755 -D zitadel $out/bin/zitadel
+        '';
 
-						meta = with lib; {
-							homepage = "https://zitadel.com/";
-							description = self.description;
-							platforms = platforms.linux;
-						};
+        meta = with lib; {
+                homepage = "https://zitadel.com/";
+                description = self.description;
+                platforms = platforms.linux;
+        };
       };
   };
 }
