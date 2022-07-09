509d0bc1531373d0000172d64ada494cca735058
thegergo02 <work@varigergo.xyz>
2022-07-09 13:28:35 +0200

    fix: overlays

--- module.nix
+++ module.nix
@@ -11,13 +11,17 @@ in {
     enable = mkOption {
       type = types.bool;
       default = false;
-      description = "Enables Zitadel";
+      description = "Enables Zitadel.";
     };
     package = mkOption {
       type = types.package;
       default = self.packages.${pkgs.system}.default;
-      description = "Zitadel package to use";
+      description = "Zitadel package to use.";
     };
+    masterKey = mkOption {
+      type = types.str;
+      description = "Master key that Zitadel uses."
+    }
   };
 
   config = mkIf cfg.enable {
