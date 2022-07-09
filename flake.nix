36a1915e9562d39534d8c740fbe8aaa93a1877ec
thegergo02 <work@varigergo.xyz>
2022-07-09 09:33:12 +0200

    fix: overlays

--- flake.nix
+++ flake.nix
@@ -12,24 +12,45 @@
         config = { allowUnfree = true; };
       };
 
-      stdenv.mkDerivation rec {
-        name = "zitadel-${version}";
-        version = "v2.0.0-v2-alpha.39";
+    stdenv.mkDerivation rec {
+      name = "zitadel-${version}";
+      version = "v2.0.0-v2-alpha.39";
 
-        src = pkgs.fetchurl {
-                url = "https://github.com/zitadel/zitadel/releases/download/${version}/zitadel_Linux_x86_64.tar.gz";
-                sha256 = "sha256-v86DTU70bOKFJ8wL6KxxlWC7ymPlywNSoWKJo+h/zHY=";
-        };
-        sourceRoot = ".";
+      src = pkgs.fetchurl {
+              url = "https://github.com/zitadel/zitadel/releases/download/${version}/zitadel_Linux_x86_64.tar.gz";
+              sha256 = "sha256-v86DTU70bOKFJ8wL6KxxlWC7ymPlywNSoWKJo+h/zHY=";
+      };
+      sourceRoot = ".";
+      
+      installPhase = ''
+      install -m755 -D zitadel $out/bin/zitadel
+      '';
+
+      meta = with lib; {
+              homepage = "https://zitadel.com/";
+              description = self.description;
+              platforms = platforms.linux;
+      };
+    };
+    overlays.default = self.overlay;
+    nixosModules.zitadel = 
+      { lib, options, config, ...  }: 
+        with lib;
+      {
+        nixpkgs.overlays = [ self.overlay ];
         
-        installPhase = ''
-        install -m755 -D zitadel $out/bin/zitadel
-        '';
+        options.services.zitadel = {
+          enable = mkOption {
+            type = types.bool;
+            default = false;
+            description = "Enables Zitadel.";
+          };
+        };
 
-        meta = with lib; {
-                homepage = "https://zitadel.com/";
-                description = self.description;
-                platforms = platforms.linux;
+        systemd.services.zitadel = {
+          description = "Starts Zitadel.";
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig.ExecStart = "${pkgs.zitadel}/bin/zitadel start-from-init";
         };
       };
   };
